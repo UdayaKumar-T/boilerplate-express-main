975ec67556f0b04e4886a0bd5280f01a5bf85438
Udayakumar <tudaya.kumar@hotmail.com>
2023-12-11 20:33:53 +0530

    Changes after adding the property file read

--- myApp.js
+++ myApp.js
@@ -3,6 +3,21 @@ let app = express();
 
 
 
+console.log("Hello World");
+/*app.get("/",function(req, res) {
+    res.send('Hello Express');
+  });*/
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    })
+
+app.get("/json", (req, res) => {
+let message = "Hello json";
+(process.env.MESSAGE_STYLE == "uppercase") ? message=message.toUpperCase() : message=message; res.json({"message": message});
+});
 
 
 
